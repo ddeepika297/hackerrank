d13b1c56d9d7e6cd74d0e7c1bd457ccd9bc86a94
ddeepika.297 <ddeepika.297@gmail.com>
2023-02-09 00:41:16 +0530

    controller

--- src/main/java/com/hackerrank/sample/controller/SampleController.java
+++ src/main/java/com/hackerrank/sample/controller/SampleController.java
@@ -1,8 +1,38 @@
 package com.hackerrank.sample.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hackerrank.sample.dto.StringResponse;
 
 @RestController
 public class SampleController {
+	@GetMapping(value = "/defaultHello")
+	public ResponseEntity getHelloMessage(@RequestParam(required = false) String message) {
+		String finalmessage = "Hello World!";
+		if (!message.isEmpty())
+			finalmessage = "Hello " + message;
+		StringResponse str = new StringResponse(finalmessage);
+		ResponseEntity res = new ResponseEntity(str, HttpStatus.OK);
+		return res;
 
+	}
+
+	@PostMapping(value = "/customHello")
+	public ResponseEntity PostHelloMessage(@RequestParam(required = false) String message) {
+
+		String finalmessage = "Hello World!";
+		if (!message.isEmpty()) {
+			finalmessage = "Hello " + message;
+			StringResponse str = new StringResponse(finalmessage);
+			ResponseEntity res = new ResponseEntity(str, HttpStatus.OK);
+			return res;
+		}
+		else return new ResponseEntity( HttpStatus.BAD_REQUEST);
+
+	}
 }
